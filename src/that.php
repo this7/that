79ad2295662f0ec28f1d62438dda9331dc8a4a4f
qinuoyun <qinuoyun@qq.com>
2018-07-30 13:46:00 +0800

    基础版本

---
+++ src/that.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * this7 PHP Framework
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2016-2018 Yan TianZeng<qinuoyun@qq.com>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.ub-7.com
+ */
+namespace this7\that;
+
+/**
+ * 应用内调
+ */
+class that {
+
+}
